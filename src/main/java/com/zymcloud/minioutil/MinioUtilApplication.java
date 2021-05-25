a62cec5b7da87fc95e0d26df7a27bb85d6608755
zhaoyimeng <zhaoyimeng@bbdservice.com>
2021-05-25 09:45:27 +0800

    init

---
+++ src/main/java/com/zymcloud/minioutil/MinioUtilApplication.java
@@ -0,0 +1,13 @@
+package com.zymcloud.minioutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinioUtilApplication.class, args);
+    }
+
+}
